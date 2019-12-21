1d7d44d46ed5c8f1d4385123a0b8600a8dff1301
tomas fleiderman <tomas.fleiderman@gmail.com>
2019-12-21 12:50:05 +0100

    changing params

--- temperaturePredictor.js
+++ temperaturePredictor.js
@@ -2,21 +2,11 @@ const brain = require('brain.js');
 
 const net = new brain.NeuralNetwork()
 
-//net.train([
-//  { input: { r: 0.03, g: 0.7, b: 0.5 }, output: { black: 1 } },
-//  { input: { r: 0.16, g: 0.09, b: 0.2 }, output: { white: 1 } },
-//  { input: { r: 0.5, g: 0.5, b: 1.0 }, output: { white: 1 } },
-//])
-
-//const output = net.run({ r: 1, g: 0.4, b: 0 }) // { white: 0.99, black: 0.002 }
-//console.log(output);
-
 net.train([
-  { input: { r: 0.03, g: 0.7 }, output: { black: 1 } },
-  { input: { r: 0.16, b: 0.2 }, output: { white: 1 } },
-  { input: { r: 0.5, g: 0.5, b: 1.0 }, output: { white: 1 } },
+  { input: { temperatura:27, humedad:70 }, output: { calor: 1 } },
+  { input: { temperatura:2, humedad:20 }, output: { calor: 0 } },
+  { input: { temperatura:33, humedad:90 }, output: { calor: 1 } },
 ])
 
-const output = net.run({ r: 1, g: 0.4, b: 0 }) // { white: 0.81, black: 0.18 }
-
+const output = net.run({ temperatura : 1, humedad: 20 }) 
 console.log(output);
